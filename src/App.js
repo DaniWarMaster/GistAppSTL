e4c2cc98cdb1b012532badaf00fcd44224352283
DaniWarMaster <danib.2008@yahoo.com>
2022-11-21 19:12:00 +0200

    Initial Commit _ Setup & UserInput

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const [updated, setUpdated] = useState('');
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      // 👇 Get input value
+      setUpdated(message);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+            type="text"
+            id="message"
+            name="message"
+            value={message}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+         />
+        <h2>Username Searched: {updated}</h2>
       </header>
     </div>
   );
